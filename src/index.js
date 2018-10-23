c8903f27cf9e315f02b9a53c38104fb80f4bd0ec
Fernando Diaz <f.diazlaclaustra@umlaud.es>
2018-10-23 10:25:09 +0200

    Convierte en constantes del modulo antiguas propiedades del singleton.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
+const K = 1;
+const K0 = 0.9996;
+const DRAD = Math.PI / 180;
+const DIAGRAPH_LETTERS_E = "ABCDEFGHJKLMNPQRSTUVWXYZ";
+const DIAGRAPH_LETTERS_N = "ABCDEFGHJKLMNPQRSTUV";
+const DIAGRAPH_LETTERS_ALL = "ABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUV";
+
+
+
+
 class Geocon {
 
     constructor (index) {
-      //constants used in calculations
-      this.k: 1,
-      this.k0: 0.9996,
-      this.drad: Math.PI / 180,
-      this.digraphLettersE: "ABCDEFGHJKLMNPQRSTUVWXYZ",
-      this.digraphLettersN: "ABCDEFGHJKLMNPQRSTUV",
-      this.digraphLettersAll: "ABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUV",
       this.datumTable: [
         { eqRad: 6378137.0, flat: 298.2572236 },    // WGS 84
         { eqRad: 6378137.0, flat: 298.2572236 },    // NAD 83
@@ -26,11 +29,11 @@ class Geocon {
       ],
 
 
-      var datum = this.datumTable[index];
+      this.datum = this.datumTable[index];
       // equatorial radius in meters
-      this.a = datum.eqRad;
+      this.a = this.datum.eqRad;
       // polar flattening
-      this.f = 1 / datum.flat;
+      this.f = 1 / this.datum.flat;
       // polar radius in meters
       this.b = this.a * (1 - this.f);
       // eccentricity
@@ -56,8 +59,8 @@ class Geocon {
      *
      */
     latLngToUtm: function (lat, lngd) {
-        var phi = lat * this.drad;                              // convert latitude to radians
-        var lng = lngd * this.drad;                             // convert longitude to radians
+        var phi = lat * DRAD;                              // convert latitude to radians
+        var lng = lngd * DRAD;                             // convert longitude to radians
         var utmz = 1 + Math.floor((lngd + 180) / 6);            // longitude to utm zone
         var zcm = 3 + 6 * (utmz - 1) - 180;                     // central meridian of a zone
         var latz = 0;                                           // this gives us zone A-B for below 80S
@@ -77,7 +80,7 @@ class Geocon {
         var N = this.a / Math.sqrt(1 - Math.pow(this.e * Math.sin(phi), 2));
         var T = Math.pow(Math.tan(phi), 2);
         var C = e0sq * Math.pow(Math.cos(phi), 2);
-        var A = (lngd - zcm) * this.drad * Math.cos(phi);
+        var A = (lngd - zcm) * DRAD * Math.cos(phi);
 
         // calculate M (USGS style)
         M = phi * (1 - esq * (1 / 4 + esq * (3 / 64 + 5 * esq / 256)));
@@ -90,11 +93,11 @@ class Geocon {
 
         // now we are ready to calculate the UTM values...
         // first the easting
-        var x = this.k0 * N * A * (1 + A * A * ((1 - T + C) / 6 + A * A * (5 - 18 * T + T * T + 72 * C - 58 * e0sq) / 120)); //Easting relative to CM
+        var x = K0 * N * A * (1 + A * A * ((1 - T + C) / 6 + A * A * (5 - 18 * T + T * T + 72 * C - 58 * e0sq) / 120)); //Easting relative to CM
         x = x + 500000; // standard easting
 
         // now the northing
-        y = this.k0 * (M - M0 + N * Math.tan(phi) * (A * A * (1 / 2 + A * A * ((5 - T + 9 * C + 4 * C * C) / 24 + A * A * (61 - 58 * T + T * T + 600 * C - 330 * e0sq) / 720))));    // first from the equator
+        y = K0 * (M - M0 + N * Math.tan(phi) * (A * A * (1 / 2 + A * A * ((5 - T + 9 * C + 4 * C * C) / 24 + A * A * (61 - 58 * T + T * T + 600 * C - 330 * e0sq) / 720))));    // first from the equator
         yg = y + 10000000;  //yg = y global, from S. Pole
         if (y < 0) {
             y = 10000000 + y;   // add in false northing if south of the equator
@@ -111,7 +114,7 @@ class Geocon {
             nato: {
                 easting: Math.round(10*(x-100000*Math.floor(x/100000)))/10,
                 northing: Math.round(10*(y-100000*Math.floor(y/100000)))/10,
-                latZone: this.digraphLettersN[latz],
+                latZone: DIAGRAPH_LETTERS_N[latz],
                 lngZone: utmz,
                 digraph: digraph
             }
@@ -144,9 +147,9 @@ class Geocon {
         var M = 0;
 
         if (!southern)
-            M = M0 + y / this.k0;    // Arc length along standard meridian.
+            M = M0 + y / K0;    // Arc length along standard meridian.
         else
-            M = M0 + (y - 10000000) / this.k;
+            M = M0 + (y - 10000000) / K;
 
         var mu = M / (this.a * (1 - esq * (1 / 4 + esq * (3 / 64 + 5 * esq / 256))));
         var phi1 = mu + e1 * (3 / 2 - 27 * e1 * e1 / 32) * Math.sin(2 * mu) + e1 * e1 * (21 / 16 - 55 * e1 * e1 / 32) * Math.sin(4 * mu);   //Footprint Latitude
@@ -155,14 +158,14 @@ class Geocon {
         var T1 = Math.pow(Math.tan(phi1), 2);
         var N1 = this.a / Math.sqrt(1 - Math.pow(this.e * Math.sin(phi1), 2));
         var R1 = N1 * (1 - Math.pow(this.e, 2)) / (1 - Math.pow(this.e * Math.sin(phi1), 2));
-        var D = (x - 500000) / (N1 * this.k0);
+        var D = (x - 500000) / (N1 * K0);
         var phi = (D * D) * (1 / 2 - D * D * (5 + 3 * T1 + 10 * C1 - 4 * C1 * C1 - 9 * e0sq) / 24);
         phi = phi + Math.pow(D, 6) * (61 + 90 * T1 + 298 * C1 + 45 * T1 * T1 - 252 * e0sq - 3 * C1 * C1) / 720;
         phi = phi1 - (N1 * Math.tan(phi1) / R1) * phi;
 
-        var lat = Math.floor(1000000 * phi / this.drad) / 1000000;
+        var lat = Math.floor(1000000 * phi / DRAD) / 1000000;
         var lng = D * (1 + D * D * ((-1 - 2 * T1 - C1) / 6 + D * D * (5 - 2 * C1 + 28 * T1 - 3 * C1 * C1 + 8 * e0sq + 24 * T1 * T1) / 120)) / Math.cos(phi1);
-        lng = lngd = zcm + lng / this.drad;
+        lng = lngd = zcm + lng / DRAD;
 
         return { lat: lat, lng: lng };
     },
@@ -221,8 +224,8 @@ class Geocon {
         if (nltr == "W" || nltr == "X" || nltr == "Y" || nltr == "Z")
             throw "W, X, Y and Z are not legal second characters in a digraph";
 
-        var eidx = this.digraphLettersE.indexOf(eltr);
-        var nidx = this.digraphLettersN.indexOf(nltr);
+        var eidx = DIAGRAPH_LETTERS_E.indexOf(eltr);
+        var nidx = DIAGRAPH_LETTERS_N.indexOf(nltr);
 
         if (utmz / 2 == Math.floor(utmz / 2)) {
             nidx -= 5;  // correction for even numbered zones
@@ -230,7 +233,7 @@ class Geocon {
 
         var ebase = 100000*(1 + eidx - 8 * Math.floor(eidx / 8));
 
-        var latBand = this.digraphLettersE.indexOf(latz);
+        var latBand = DIAGRAPH_LETTERS_E.indexOf(latz);
         var latBandLow = 8 * latBand - 96;
         var latBandHigh = 8 * latBand - 88;
 
@@ -249,9 +252,9 @@ class Geocon {
         var highLetter = Math.round(100 + 1.11 * latBandHigh);
         var latBandLetters = null;
         if (utmz / 2 == Math.floor(utmz / 2)) {
-            latBandLetters = this.digraphLettersAll.slice(lowLetter + 5, highLetter + 5);
+            latBandLetters = DIAGRAPH_LETTERS_ALL.slice(lowLetter + 5, highLetter + 5);
         } else {
-            latBandLetters = this.digraphLettersAll.slice(lowLetter, highLetter);
+            latBandLetters = DIAGRAPH_LETTERS_ALL.slice(lowLetter, highLetter);
         }
         var nbase = 100000 * (lowLetter + latBandLetters.indexOf(nltr));
 
@@ -291,9 +294,9 @@ class Geocon {
         var M0 = 0;
 
         if (!southern)
-            M = M0 + y / this.k0;    // Arc length along standard meridian.
+            M = M0 + y / K0;    // Arc length along standard meridian.
         else
-            M = M0 + (y - 10000000) / this.k;
+            M = M0 + (y - 10000000) / K;
 
         //
         // calculate the latitude so that we can derive the latitude zone
@@ -305,11 +308,11 @@ class Geocon {
         var T1 = Math.pow(Math.tan(phi1), 2);
         var N1 = this.a / Math.sqrt(1 - Math.pow(this.e * Math.sin(phi1), 2));
         var R1 = N1 * (1 - Math.pow(this.e, 2)) / (1 - Math.pow(this.e * Math.sin(phi1), 2));
-        var D = (x - 500000) / (N1 * this.k0);
+        var D = (x - 500000) / (N1 * K0);
         var phi = (D * D) * (1 / 2 - D * D * (5 + 3 * T1 + 10 * C1 - 4 * C1 * C1 - 9 * e0sq) / 24);
         phi = phi + Math.pow(D, 6) * (61 + 90 * T1 + 298 * C1 + 45 * T1 * T1 - 252 * e0sq - 3 * C1 * C1) / 720;
         phi = phi1 - (N1 * Math.tan(phi1) / R1) * phi;
-        var lat = Math.floor(1000000 * phi / this.drad) / 1000000;
+        var lat = Math.floor(1000000 * phi / DRAD) / 1000000;
 
         // convert latitude to latitude zone for nato
         if (lat > -80 && lat < 72) {
@@ -327,7 +330,7 @@ class Geocon {
         return {
             easting: x,
             northing: y,
-            latZone: this.digraphLettersN[latz],
+            latZone: DIAGRAPH_LETTERS_N[latz],
             lngZone: utmz,
             digraph: digraph
         }
@@ -351,12 +354,12 @@ class Geocon {
         //
         var letter = Math.floor((utmz - 1) * 8 + (x) / 100000);
         letter = letter - 24 * Math.floor(letter / 24) - 1;
-        var digraph = this.digraphLettersE.charAt(letter);
+        var digraph = DIAGRAPH_LETTERS_E.charAt(letter);
 
         letter = Math.floor(y / 100000);
         if (utmz / 2 == Math.floor(utmz / 2)) { letter = letter + 5; }
         letter = letter - 20 * Math.floor(letter / 20);
-        digraph = digraph + this.digraphLettersN.charAt(letter);
+        digraph = digraph + DIAGRAPH_LETTERS_N.charAt(letter);
 
         return digraph;
     }
